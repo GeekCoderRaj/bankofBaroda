d0612b91004be4a2b36a7bab62d2220906fd12d1
AKHIL RAJ SRIVASTAVA <84132589+AKHILRAJSRIVASTAVA@users.noreply.github.com>
2022-10-12 16:44:20 +0530

    azure ocr api

---
+++ abcd.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify, Response
+import cv2 as cv
+import os
+import m
+
+app = Flask(__name__)
+
+
+def break_image():
+   img=cv.imread("image.jpeg")
+   img=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+   edged = cv.Canny(img, 50, 500)
+   contours, hierarchy = cv.findContours(edged, 
+    cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
+   cv.drawContours(img, contours, -1, (0, 255, 0), 2) 
+   size=img.shape
+   datebox=img[0:size[0]//5,size[1]-(size[1]//4):size[1]]
+   payee_name=img[size[0]//6:size[0]//2,0:size[1]-(size[1]//5)]
+   bottom_box=img[size[0]-(size[0]//5):size[0],size[1]//5:size[1]-size[1]//5]
+   amt_box=img[size[0]//3:size[0]//2,size[1]-(size[1]//3):size[1]]
+   bank_name=img[0:size[0]//5,0:size[1]-(size[1]//3)]
+   ac_no=img[size[0]//2:size[0]-(size[0]//5*2),0:size[1]]
+   sign_box=img[size[0]//2:size[0]-(size[0]//6),size[1]-(size[1]//3):size[1]]
+   parent_dir=os.getcwd()
+   image_dir="Images"
+   path=os.path.join(parent_dir,image_dir)
+   os.mkdir(path)
+   cv.imwrite(os.path.join(path , 'Date.jpeg'), datebox)
+   Date = m.azure_ocr_api("Date.jpeg")
+
+   cv.imwrite(os.path.join(path , 'sign.jpeg'),sign_box)
+   sign = m.azure_ocr_api("sign.jpeg")
+   
+   cv.imwrite(os.path.join(path , 'Payee_name.jpeg'),payee_name)
+   Payee_name = m.azure_ocr_api("Payee_name.jpeg")
+   
+   cv.imwrite(os.path.join(path , 'Account_no.jpeg'),ac_no)
+   Account_no = m.azure_ocr_api("Account_no.jpeg")
+   
+   cv.imwrite(os.path.join(path , 'Bank_name.jpeg'),bank_name)
+   Bank_name = m.azure_ocr_api("Bank_name.jpeg")
+   
+   cv.imwrite(os.path.join(path , 'Bottom_digits.jpeg'),bottom_box)
+   Bottom_digits = m.azure_ocr_api("Bottom_digits.jpeg")
+   
+   cv.imwrite(os.path.join(path , 'Amount.jpeg'),amt_box)
+   Amount = m.azure_ocr_api("Amount.jpeg")
+   
+   
+
+@app.route("/im_size", methods=["POST"])
+def process_image():
+    file = request.files["image"]
+    print(file)
+
+    
+    # Read the image via file.stream
+    # img = Image.open(file.stream)
+    file.save(f'image.jpeg')
+    break_image()
+    return Response(status=200)
+    # return jsonify({'msg': 'success'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ m.py
@@ -0,0 +1,44 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+import json
+
+
+subscription_key = ""
+endpoint = ""
+ 
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+def azure_ocr_api(image_url):
+        local_image_url = "Cheque083654.jpeg"
+        read_response = computervision_client.read_in_stream(open("./Images/" + image_url,'rb'),  raw=True)
+        #read_response = computervision_client.read_in_stream(open(local_image_url,'rb'),  raw=True)
+
+        # Get the operation location (URL with an ID at the end) from the response
+        read_operation_location = read_response.headers["Operation-Location"]
+        # Grab the ID from the URL
+        operation_id = read_operation_location.split("/")[-1]
+
+        # Call the "GET" API and wait for it to retrieve the results 
+        while True:
+            read_result = computervision_client.get_read_result(operation_id)
+            if read_result.status not in ['notStarted', 'running']:
+                break
+            time.sleep(1)
+        list = []
+        if read_result.status == OperationStatusCodes.succeeded:
+            for text_result in read_result.analyze_result.read_results:
+
+                for line in text_result.lines:
+                    list.append(line.text)
+        # print(list)
+        # pass
+        return list
+# azure_ocr_api()        
+print("End of Computer Vision quickstart.")
